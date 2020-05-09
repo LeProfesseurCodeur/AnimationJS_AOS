d349387c840ddb1438a80e7a5bf769b973ba1f0d
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-05-09 15:07:02 +0200

    first init

---
+++ app.js
@@ -0,0 +1,5 @@
+AOS.init({
+    offset: 400,    // décalage (en px) par rapport au point de déclenchement d'origine
+    delay: 0,       // valeurs de 0 à 3000, avec pas de 50 ms
+    duration: 1000  // valeurs de 0 à 3000, avec pas de 50 ms
+});
